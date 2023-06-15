051128633da0bf3b7cc62e5d9dc75619a1128203
khaled <me@me.com>
2023-06-15 04:10:45 -0700

    1

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int main(){
-char alo[] = "Programming is like building a multilingual puzzle";
+char alo[] = "\"Programming is like building a multilingual puzzle";
 puts(alo);
 return 0;
 }
