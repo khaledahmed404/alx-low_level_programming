def6204952dcaecf43d984cb5f7089b559eca377
khaled <me@me.com>
2023-06-20 03:13:19 -0700

    1

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,13 +10,15 @@ void print_alphabet_x10(void)
 	char c;
 	int i = 0;
 
-	while (i < 10)
-		for (c = 'a'; c < 'z'; ++c)
+	while (i <= 9)
+	{
+		for (c = 'a'; c <= 'z'; c++)
 		{
 			_putchar(c);
 		}
-	_putchar('\n');
 
-	i++;
-}
+		_putchar('\n');
+
+		i++;
+	}
 }
