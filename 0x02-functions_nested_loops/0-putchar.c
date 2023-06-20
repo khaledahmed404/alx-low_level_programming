a7053bb7366bbb5ccc7ec4781ebd284103a8c106
khaled <me@me.com>
2023-06-20 02:56:51 -0700

    1

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,11 +4,12 @@
   * main - Entry point
   *
   * Return: Always 0
+  *
   */
 int main(void)
 {
 	_putchar('_');
-	_putchar('p')
+	_putchar('p');
 	_putchar('u');
 	_putchar('t');
 	_putchar('c');
