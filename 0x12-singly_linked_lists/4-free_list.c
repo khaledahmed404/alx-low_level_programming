9d699aa4adfddfdd429b05e9e116a8a2ea8819f0
khaled <khaledahmed102000@gmail.com>
2023-07-27 05:23:49 -0700

    1

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_list - a fun to free linked list
+ *
+ * @head: list_t
+ */
+
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	while (head)
+	{
+
+		temp = head->next;
+		free(head->str);
+
+		free(head);
+		head = temp;
+	}
+}
